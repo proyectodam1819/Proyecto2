7094a95210d4463ec8e371769f295813f3609126
dam <proyectodam1819>
2018-11-15 09:45:18 +0100

    Nueva version

---
+++ app/src/main/java/org/izv/aad/proyecto/Logup.java
@@ -0,0 +1,21 @@
+package org.izv.aad.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.izv.aad.proyecto.Fragments.FragmentLogup1;
+import org.izv.aad.proyecto.Fragments.FragmentLogup2;
+
+public class Logup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logup);
+
+        FragmentLogup1 fragmentLogup1 = (FragmentLogup1)getSupportFragmentManager().findFragmentById(R.id.fragmento1);
+        FragmentLogup2 fragmentLogup2 = (FragmentLogup2)getSupportFragmentManager().findFragmentById(R.id.fragment2);
+
+        fragmentLogup2.visi
+    }
+}
